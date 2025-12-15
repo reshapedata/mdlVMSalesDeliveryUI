3d98273eef669d6a01cc84fd2aec330ce3e12868
takewiki <hulilei@takeiwki.com.cn>
2025-12-15 01:30:56 +0000

    v1.0.3

--- R/DeliveryLocation.R
+++ R/DeliveryLocation.R
@@ -1,3 +1,26 @@
+library(shiny)
+library(DT)
+library(shinyWidgets)
+DeliveryLocation_all_columns_ui <- c(
+  'Delivery Location',
+  'Sales OrderID',
+  'Sales OrderDate',
+  'PN',
+  'Product Name',
+  'Sales OrderQty',
+  'Delivery Date',
+  'Delivery Qty'
+)
+#设置默认值
+DeliveryLocation_default_columns_ui <- c(
+  'Delivery Location',
+  'Sales OrderID',
+  'Sales OrderDate',
+  'PN',
+  'Product Name',
+  'Sales OrderQty'
+)
+
 #' 交货地点生成器界面
 #'
 #' @param colTitles  主页标题
@@ -42,7 +65,11 @@ DeliveryLocationUI_left <- function() {
 
 
   res <- tagList(
-    tsui::mdl_text2(id ='text_DeliveryLocation' ,label ='Delivery Location',value = '')
+    tsui::mdl_text2(id ='text_DeliveryLocation' ,label ='Delivery Location',value = ''),
+
+    shiny::actionButton(inputId = 'btn_DeliveryLocation_view',label = 'Search'),
+
+    tsui::mdl_download_button(id = 'dl_DeliveryLocation',label = 'Download')
 
 
 
@@ -66,11 +93,34 @@ DeliveryLocationUI_left <- function() {
 DeliveryLocationUI_right <- function() {
   res <- tagList(
 
+    pickerInput(
+      inputId = "DeliveryLocation_column_selector",
+      label = "Selected Columns:",
+      choices = DeliveryLocation_all_columns_ui,
+      selected = DeliveryLocation_default_columns_ui,
+      options = list(
+        `actions-box` = TRUE,
+        `selected-text-format` = "count > 3",
+        `count-selected-text` = "{0} Columns Seleced",
+        size = 10,
+        `live-search` = TRUE,
+        `live-search-style` = "contains"
+      ),
+      multiple = TRUE,
+      choicesOpt = list(
+        style = rep(("color: black;"), 100)
+      )
+    ),
+    # 全选/取消全选按钮
+    actionButton("btn_DeliveryLocation_select_all", "Select All",
+                 class = "btn-primary btn-sm",
+                 style = "margin-right: 5px;"),
+    actionButton("btn_DeliveryLocation_deselect_all", "Deselect All",
+                 class = "btn-secondary btn-sm"),
+    actionButton("btn_DeliveryLocation_defaultValue", "Default Value",
+                 class = "btn-primary btn-sm"),
+    verbatimTextOutput("DeliveryLocation_selection_info")
 
-
-    shiny::actionButton(inputId = 'btn_DeliveryLocation_view',label = 'Search'),
-
-    tsui::mdl_download_button(id = 'dl_DeliveryLocation',label = 'Download')
 
 
 
